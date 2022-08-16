69705d364e01b52b731589f7091799f46fc6ee95
Gabriel Mendes <gabrielx165@gmail.com>
2022-08-16 08:38:42 -0300

    removed required prop from user model

--- UserRegistration/Models/User.cs
+++ UserRegistration/Models/User.cs
@@ -5,14 +5,12 @@ namespace UserRegistration.Models
 {
     public class User
     {
-        [Required]
         public string Id { get; private set; } = System.Guid.NewGuid().ToString();
         [Required]
         public string Name { get; set; }
         public string? SurName { get; set; }
         [Required]
         public int Age { get; set; }
-        // [Required]
         public DateTime CreationDate { get; private set; } = DateTime.UtcNow;
     }
 }
